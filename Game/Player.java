450451f472baf4d135e5e42d2bd289981054e4c1
JaviCan1 <j.canizares.garcia@student.tue.nl>
2024-10-17 12:20:14 +0200

    Added function to reward player when he kills an enemy  (void playerReward(long xpWorth, goldWorth))

--- Game/Player.java
+++ Game/Player.java
@@ -7,12 +7,18 @@ public class Player extends GCharacter {
     Heal[] heals;
 
     protected long maxHp;
+    protected long maxArmor;
     protected long armor;
     protected long nextLevel;
     protected long currXp;
+    protected long gold;
 
     long getMaxHp() {
         return maxHp;
+    }
+
+    long getMaxArmor() {
+        return maxArmor;
     }
 
     long getArmor() {
@@ -32,22 +38,30 @@ public class Player extends GCharacter {
     }
 
     public Player() {
-        level = 1;
-        hp = 250;
-        health = 250;
-        damage = 250;
 
         items = new Item[3];
         items[0] = new Hand();
         items[1] = new Hand();
         items[2] = new Hand();
 
-        maxHp = hp + items[0].hp + items[1].hp + items[2].hp;
-
         heals = new Heal[3];
         heals[0] = new EmptyBottle();
         heals[1] = new EmptyBottle();
         heals[2] = new EmptyBottle();
+
+        level = 1;
+        hp = 250;
+        maxHp = hp + items[0].hp + items[1].hp + items[2].hp;
+        health = maxHp;
+        armor = 250;
+        maxArmor = armor + items[0].armor + items[1].armor + items[2].armor;
+        damage = 250;
+
+        currXp = 0;
+        nextLevel = Math.round((Math.pow(Math.E, level)-1) * Math.pow(10,4) / 75);
+        gold = 0;
+
+        
     }
 
     void changeItem(int n, Item i) {
@@ -62,9 +76,44 @@ public class Player extends GCharacter {
         return heals[slot];
     }
 
+    void playerReward(long xpWorth, long goldWorth) {
+        boolean lvlUp = false;
+        long xp = xpWorth;
+        long oldHp = hp;
+
+        currXp = currXp + xp;
+        while (currXp >= nextLevel) {
+            xp = xp - (nextLevel- currXp);
+            level = level  + 1;
+            nextLevel = Math.round((Math.pow(Math.E, level)-1) * Math.pow(10,4) / 75);
+            currXp = 0;
+            lvlUp = true;
+        }
+        
+        if (lvlUp) {
+            currXp = xp;
+
+            hp = Math.round((Math.pow(Math.E, 0.05 * (level+5)) - 1) * Math.pow(10,4));
+            armor = Math.round((Math.pow(Math.E, 0.05 * (level+3)) - 1) * Math.pow(10,4) / 10);
+            damage = Math.round((Math.pow(Math.E, 0.05 * (level)) - 1) * Math.pow(10,4));
+            maxHp = hp + items[0].hp + items[1].hp + items[2].hp;
+            // health = health + ((hp-oldHp) / 4);
+            maxArmor = armor + items[0].armor + items[1].armor + items[2].armor;
+
+        }
+        gold = gold + goldWorth;
+
+    }
+
         //overriting (dont remember name of this in OOP)
-        void lowerHp(int dmg) {
-            this.health -= (dmg - (armor + items[0].armor + items[1].armor + items[2].armor));
+        void lowerHp(long dmg) {
+            long realDamage = dmg - maxArmor;
+            if (realDamage < 0){
+                realDamage = 0;
+
+            }
+            
+            this.health -= realDamage;
             if (this.health < 0) {
                 this.health = 0;
             }
